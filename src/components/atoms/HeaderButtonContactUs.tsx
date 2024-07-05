6e638bedf92a7aaf9e0b1874193327f226db92f8
ACH RIZAL <achrizal15@gmail.com>
2024-07-06 02:34:24 +0700

    Darkmode

--- src/components/atoms/HeaderButtonContactUs.tsx
+++ src/components/atoms/HeaderButtonContactUs.tsx
@@ -21,9 +21,9 @@ const HeaderButtonContactUs: FC<HeaderButtonContactUsProps> = ({ className }) =>
         };
     }, []);
     return (
-        <a href="http://" target="_blank" rel="noopener noreferrer" aria-label="contact us" className={`relative inline-block px-6 py-3 text-blue-700 border-2 border-blue-700 transition-transform duration-500 ease-out group overflow-hidden rounded-full ${!scrolled && 'shadow-xl'} ${className}`}
+        <a href="http://" target="_blank" rel="noopener noreferrer" aria-label="contact us" className={`relative inline-block px-6 py-3 text-blue-800 border-2 border-blue-800 dark:text-gray-500 dark:border-gray-500 transition-transform duration-500 ease-out group overflow-hidden rounded-full ${!scrolled && 'shadow-xl'} ${className}`}
         >
-            <span className={`absolute inset-0 w-full h-full transition-all duration-500 ease-out transform scale-0 bg-gradient-to-tl from-blue-600 to-blue-800 group-hover:scale-150 origin-bottom-right rounded-tl-full ${!scrolled && 'scale-150'}`}></span>
+            <span className={`absolute inset-0 w-full h-full transition-all duration-500 ease-out transform scale-0 bg-blue-800 group-hover:scale-150 dark:bg-gray-900 origin-bottom-right rounded-tl-full ${!scrolled && 'scale-150'}`}></span>
             <span className={`relative z-10 transition-colors flex items-center gap-2 duration-500 ease-out group-hover:text-white font-medium font-roboto ${!scrolled && 'text-white'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" className='text-2xl' viewBox="0 0 24 24">
                     <path fill="currentColor" d="M6.62 10.79c1.44 2.83 3.76 5.15 6.59 6.59l2.2-2.2c.28-.28.67-.36 1.02-.25c1.12.37 2.32.57 3.57.57a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A17 17 0 0 1 3 4a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02z" />
--- src/components/atoms/NavLink.tsx
+++ src/components/atoms/NavLink.tsx
@@ -30,8 +30,8 @@ const NavLink: React.FC<NavLinkProps> = ({ href, children,scrolledDefault=false,
       <a
         href={href}
         className={`font-medium border-2 px-7 py-3 rounded-full ${scrolled 
-          ? (`hover:text-white hover:border-blue-800 hover:bg-blue-800 
-            ${isActive ? 'text-white border-blue-800 bg-blue-800':'border-transparent text-gray-800'}`)
+          ? (`hover:text-white hover:border-blue-800 hover:bg-blue-800  dark:border-transparent dark:hover:bg-gray-900 dark:hover:border-gray-800 
+            ${isActive ? 'text-white dark:bg-gray-900 dark:text-white border-blue-800 bg-blue-800':'border-transparent dark:text-gray-500 text-gray-800'}`)
           : (`text-white hover:border-white hover:backdrop-blur-sm hover:bg-white/10 
             ${isActive ? 'border-white backdrop-blur-sm bg-white/10':'border-transparent'}`)} transition-all duration-300`}
       >
